2e014e1ddbe23cdb597f1fec1329a3efbf2ef8c4
Cristiano de Paula <crdepa@gmail.com>
2024-09-16 16:14:24 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>Apontador</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Apontador para Redes Sociais">    
+    <meta name="keywords" content="Rede Concepcionista de Ensino, Educação, Escolas">
+    <meta name="author" content="Cristiano de Paula">    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" href="images/logo.ico" type="image/x-icon">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" type="text/css" href="css/slick.css">
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+    <script src="https://cdn.tailwindcss.com"></script>    
+</head>
+
+<body>
+    <div class="w-[1080px] h-[1920px] bg-cover bg-center" style="background-image: url('images/fundo.png');">
+        <img class="m-auto pt-20" src="images/logo.png" alt="Logo" />
+        <div>
+            <div>
+                <p class="py-24 text-white text-7xl text-center"><b>Colégio Maria Imaculada</b></p>
+            </div>
+            <div>
+                <a href="https://www.cmisp.com.br">
+                    <img class="m-auto py-2" src="images/BOTAO-SITE.png" />
+                </a>
+            </div>
+            <div>
+                <a href="https://youtu.be/zsFO2EX8kKg">
+                    <img class="m-auto py-2" src="images/BOTAO-TOUR.png" />
+                </a>
+            </div>
+            <div>
+                <a href="http://www.cmisp.com.br/facaparte/index.php">
+                    <img class="m-auto py-2" src="images/BOTAO-MATRICULAS.png" />
+                </a>
+            </div>
+            <div>
+                <a href="https://www.facebook.com/pages/CMISP/162482197249924?fref=ts">
+                    <img class="m-auto py-2" src="images/BOTAO-FACEBOOK.png" />
+                </a>
+            </div>
+            <div>
+                <a href="https://www.youtube.com/user/cmispcolegio">
+                    <img class="m-auto py-2" src="images/BOTAO-YOUTUBE.png" />
+                </a>
+            </div>
+            <div>
+                <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5511947978925">
+                    <img class="m-auto py-2" src="images/BOTAO-WHATSAPP.png" />
+                </a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
